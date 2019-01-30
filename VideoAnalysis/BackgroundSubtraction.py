4037987d910de2192167a4d2d55bc18a2ffd35d0
Santhosh B <Santhosh.B@efi.com>
2019-01-30 08:32:16 +0530

    First Commit

---
+++ VideoAnalysis/BackgroundSubtraction.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 23:35:46 2019
+
+@author: santhob
+"""
+
+import cv2
+import cv2.bgsegm
+
+cap = cv2.VideoCapture('kidsvideo.mp4')
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+
+while(1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow('frame',fgmask)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
